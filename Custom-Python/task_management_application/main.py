0a52f41b93af2c51dcec2ed9178b1842a1e8346d
albertverso <caralbertverso@gmail.com>
2024-06-24 00:50:26 -0300

    in progress 1

--- Custom-Python/task_management_application/main.py
+++ Custom-Python/task_management_application/main.py
@@ -18,29 +18,29 @@ cursor = connection_db.cursor()
 
 class Crud:
     #CREATE
-    def insert_user(self, nome, email, creatAt):
+    def insert_user(self, nome, email, createdAt):
         self.nome = nome
         self.email = email
-        self.createdAt = creatAt
+        self.createdAt = createdAt
 
         command = f'INSERT INTO user (name, email, createdAt) VAlUES ("{self.nome}", "{self.email}", "{self.createdAt}")'
         cursor.execute(command)
         connection_db.commit() #edita o banco de dados 
 
-    def insert_task(self, title, description, creatAt, completedAt, userId):
+    def insert_task(self, title, description, createdAt, userId):
         self.title = title
         self.description = description
-        self.createdAt = creatAt
+        self.createdAt = createdAt
         self.userId = userId
         
-        command = f'INSERT INTO task (title, description, creatAt, userId) VAlUES ("{self.title}", "{self.description}", "{self.createdAt}", "{self.userId}")'
+        command = f'INSERT INTO task (title, description, createdAt, userId) VAlUES ("{self.title}", "{self.description}", "{self.createdAt}", "{self.userId}")'
         cursor.execute(command)
         connection_db.commit() #edita o banco de dados 
 
     #READ
-    def read():
+    def read(choosen):
 
-        command = f'SELECT * FROM User;'
+        command = f'SELECT * FROM {choosen};'
         cursor.execute(command)
         result = cursor.fetchall() #le o banco de dados
 
@@ -54,22 +54,49 @@ class Crud:
 
         return result
         
+    def read_task(user_id):
+
+        command = f'SELECT * FROM Task Where userId = "{user_id}";'
+        cursor.execute(command)
+        result = cursor.fetchall() #le o banco de dados
+      
+        return result
+    
+        
     #UPDATE
-    def update(coluna ,choosen, email):
+    def update_user(coluna ,choosen, email):
 
         command = f'UPDATE User SET {coluna} = "{choosen}" WHERE email = "{email}"'
         cursor.execute(command)
         connection_db.commit() #edita o banco de dados
+        
+    def update_task(coluna ,choosen, title):
+
+        command = f'UPDATE Task SET {coluna} = "{choosen}" WHERE userId = "{title}"'
+        cursor.execute(command)
+        connection_db.commit() #edita o banco de dados
 
     #DELETE
-    def delete(choosen):
-
+    def delete_user(choosen):
+       
         command = f'DELETE FROM User WHERE email = "{choosen}" '
         cursor.execute(command)
         connection_db.commit() #edita o banco de dados 
 
+        print("\n Usuário deletado com sucesso!!!")
+        
+    def delete_task(choosen):
 
-def create(option = 1, user_id = 0):    
+        command = f'DELETE FROM Task WHERE title = "{choosen}" '
+        cursor.execute(command)
+        connection_db.commit() #edita o banco de dados 
+
+        print("\n Tarefa deletada com sucesso!!!")
+
+
+
+
+def create(option = 0, user_id = 0):    
     created = Crud() 
 
     if option == 1:
@@ -79,26 +106,91 @@ def create(option = 1, user_id = 0):
 
         created.insert_user(nome, email, data_criacao)
 
-        print("Usuário adicionado com sucesso!!!")
+        print("\n Usuário adicionado com sucesso!!!")
     else:
-        title = input("digite seu nome: ")
-        description = input("digite seu email: ")
+        title = input("digite um titulo para sua tarefa: ")
+        description = input("digite uma descrição de sua tarefa: ")
         data_criacao = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        idUser = 2
+        idUser = user_id
 
-        created.insert_task(title, description, data_criacao, data_final, idUser)
+        created.insert_task(title, description, data_criacao, idUser)
 
-        print("Usuário adicionado com sucesso!!!")
+        print("\n Tarefa adicionada com sucesso!!!")
 
-def verify(email):
+def updated(option, choosen):
+    if option == 1:
+        tributes = int(input("Deseja editar nome[1] ou email[2]?"))
+        if tributes == 1:
 
-    result = Crud.read_user(email) ## PAREI PARA VERIFICAR SE EXISTE USUARIO PARA CASDASTRA UMA TAREFA
+            update_name = input("Para qual nome deseja mudar?")
+            Crud.update_user("name", update_name, choosen)
+
+            print("\n Nome do usuário atualizado com sucesso!!!")
+        elif tributes == 2:
+            update_email = input("Para qual email deseja mudar?")
+            Crud.update_user("email", update_email, choosen)
+
+            print("\n Email do usuário atualizado com sucesso!!!")
+    else:
+        user_id = choosen
+        result = Crud.read_task(user_id)
+        verify_id = 0
+
+        id_user = [usuario[4] for usuario in result]
+        for id in id_user:
+            verifi_id = id
+        # PAREI NA PARTE DE VERIFICAR SE USUARIO ESTA ESTRELADO AO TASK PARA PODER ATUALIZAR A TABELA DE TAREFAS
+
+        #ja tenho a vcerificação de id de uma task que esta atrelado ao usuario
+        print(verifi_id)
+
+        #proximo passo pergunta ao usuario o titulo da task que ele quer editar e verificar se a task que ele solicitou pertence a ele
+
+
+        # tributes = int(input("Deseja editar titulo[1] ou descrição[2]?"))
+        # if tributes == 1:
+        #     update_title = input("Digite um novo titulo: ")
+        #     Crud.update_task("title", update_title, choosen)
+
+        #     print("\n titulo atualizado com sucesso!!!")
+        # elif tributes == 2:
+        #     update_description = input("Digite uma nova descrição: ")
+        #     Crud.update_task("description", update_description, choosen)
+
+        #     print("\n descrição atualizada com sucesso!!!")
+
+def verify_user(email, type_crud = "" ,option = 0,):
+    
+    result = Crud.read_user(email)
     
     if result == []:
-        print("usuario nao encontrado")
+        return print("\n Operação inválida, usuário não existe. ")
     else:
-        print("ACHOUUUU BB")    
-  
+        if option == 1: 
+            if type_crud == "delete":
+                Crud.delete_user(email)
+            elif type_crud == "update":
+                updated(option, email)
+            elif type_crud == "read":
+                result = Crud.read_user(email)
+                print(f"\n{result}")
+
+        else:
+            id_user = [usuario[0] for usuario in result]  
+            for id in id_user: 
+                if type_crud == "create":
+                    create(option, id)
+
+                elif type_crud == "read":
+                    result = Crud.read_task(id)
+                    print(f"\n{result}") 
+
+                elif type_crud == "delete":
+                    title = input("Digite o titulo da tarefa que deseja deletar: ")
+                    Crud.delete_task(title)
+
+                elif type_crud == "update":
+                    updated(option, id)
 
     
    
@@ -119,50 +211,46 @@ def main():
         opcao = menu()
 
         if opcao == "c":
-            option = int(input("Deseja criar um novo usuario [1] ou uma nova terefa[2]?"))
+            option = int(input("Deseja criar um novo usuario [1] ou uma nova tarefa[2]?"))
             if option == 1:
-                create()
+                create(option)
             elif option == 2:
                 email = input("Digite seu email de usuário: ")
-                verify(email)
-                #create(option)
+                verify_user(email, "create", option)
             else:
-                print("\n Operação inválida, por favor selecione novamente a operação desejada. ")      
+                print("\n Operação inválida, por favor selecione novamente a operação desejada. ")   
+
         elif opcao == "r":
-            Crud.read()
-        elif opcao == "u":
-            email = input("digite seu email: ")
-            option = int(input("Deseja editar nome[1] ou email[2]?"))
-
+            option = int(input("Deseja exibir um usuario [1] ou uma tarefa[2]?"))
             if option == 1:
-
-                update_name = input("Para qual nome deseja mudar?")
-                Crud.update("name", update_name, email)
-
-                print("Nome do usuário atualizado com sucesso!!!")
+                email = input("Digite seu email de usuário: ")
+                verify_user(email, "read", option)
             elif option == 2:
-
-                update_email = input("Para qual email deseja mudar?")
-                Crud.update("email", update_email, email)
-
-                print("Email do usuário atualizado com sucesso!!!")
-
+                email = input("Digite seu email de usuário: ")
+                verify_user(email, "read", option)
             else:
-                print("\n Operação inválida, por favor selecione novamente a operação desejada. ")  
+                print("\n Operação inválida, por favor selecione novamente a operação desejada. ") 
+
+        elif opcao == "u":
+            option = int(input("Deseja atualizar um usuario [1] ou uma tarefa[2]?"))
+
+            if option == 1 or 2:
+                email = input("digite seu email: ")
+                verify_user(email, "update", option)
+            else:
+                print("\n Operação inválida, por favor selecione novamente a operação desejada. ") 
+
+            
         elif opcao == "d":
             option = int(input("Deseja deletar Usuário[1] ou Tarefa[2]?"))
 
             if option == 1:
-
                 delete = input("Digite o email de usuário que deseja deletar: ")
-                Crud.delete(delete)
-                print("Usuário deletado com sucesso!!!")
+                verify_user(delete, "delete", option)
 
             elif option == 2:
-
-                delete = input("Digite o titulo da tarefa que deseja deletar: ")
-                Crud.delete(delete)
-                print("Usuário deletado com sucesso!!!")
+                delete = input("Digite seu email: ")
+                verify_user(delete, "delete", option)
 
             else:
                 print("\n Operação inválida, por favor selecione novamente a operação desejada. ")  
