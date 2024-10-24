c8560ad5b740b651fe2fcfc4401c6e0671c06be2
Carlos Alberto <caralbertverso@gmail.com>
2024-10-24 17:21:08 -0300

    RPA tests

---
+++ Testes-RPA/assert.py
@@ -0,0 +1,4 @@
+podBayDoorStatus = 'open'
+assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+podBayDoorStatus = 'I\'m sorry, Dave. I\'m afraid I cant do that.'
+assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
